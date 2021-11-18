0a69b4899f1c0d62e5c57d20d155bd194a48beb1
Adeniyi-Bella <37347588+Adeniyi-Bella@users.noreply.github.com>
2021-11-18 09:09:23 +0100

    Object detection from video

--- yolo-3-video.py
+++ yolo-3-video.py
@@ -1,13 +1,5 @@
 
 
-"""
-Course:  Training YOLO v3 for Objects Detection with Custom Data
-
-Section-2
-Objects Detection on Video with YOLO v3 and OpenCV
-File: yolo-3-video.py
-"""
-
 
 # Detecting Objects on Video with OpenCV deep learning library
 #
